df8959df7ba7319965aaa8ae32d388016e9a8963
H-Ramalan <talktoramalan@gmail.com>
2023-09-07 12:51:16 -0700

    fix linters errors

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 require_relative 'nameable'
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
-  attr_accessor :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
 
   def initialize(age, name: 'unknown', parent_permission: true)
     super()
